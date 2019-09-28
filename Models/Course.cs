1af108225c1c50c6eb562de4d4becb31841a4eb1
SharovaOV <olyaart851@mail.ru>
2019-09-28 10:22:01 +0300

    Добавлены 2 первых спраочника

---
+++ Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProfSpec.Models
+{
+    public class Course
+    {
+        [Display(Name="Номер курса")]
+        public int CourseID { get; set; }
+
+        [Display(Name= "Наименованbt курса")]
+        public string Name { get; set; }
+    }
+}
